4a7c969ace0593162f11227c989343a4fe1b1446
KSW <rlatjsdnd325@naver.com>
2021-07-28 16:28:03 +0900

    테스트

--- BurningAndroidServer/src/main/java/controller/JoinService.java
+++ BurningAndroidServer/src/main/java/controller/JoinService.java
@@ -19,6 +19,6 @@ public class JoinService extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	}
+	} 
 
 }
