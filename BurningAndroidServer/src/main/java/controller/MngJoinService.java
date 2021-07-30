38d4fa98edb63dd08c02d47080465973ee481fd0
Ming <Ming@DESKTOP-LP9LHKT.mshome.net>
2021-07-30 10:39:06 +0900

    서비스콘 생성

---
+++ BurningAndroidServer/src/main/java/controller/MngJoinService.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MngJoinService")
+public class MngJoinService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("요청 확인");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		String tel = request.getParameter("tel");
+		String addr = request.getParameter("adr");
+		String b_name = request.getParameter("bdname");
+		
+	}
+
+}
--- BurningAndroidServer/src/main/java/controller/UserJoinService.java
+++ BurningAndroidServer/src/main/java/controller/UserJoinService.java
@@ -7,13 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/JoinService")
-public class JoinService extends HttpServlet {
+@WebServlet("/UserJoinService")
+public class UserJoinService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-	
-	} 
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		String tel = request.getParameter("tel");
+		String gen = request.getParameter("gen");
+		String bir = request.getParameter("birth");
+		
+		
+	}
 
-}  
+}
